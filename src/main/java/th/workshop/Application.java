67005ca353d22075508be537282de31248de9361
keattiwut <keattiwut.k@gmail.com>
2015-08-15 15:03:36 +0700

    add constuctor

--- src/main/java/th/workshop/Application.java
+++ src/main/java/th/workshop/Application.java
@@ -9,8 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args){
+    private Application(){
 
+    }
+
+    public static void main(String[] args){
         SpringApplication.run(Application.class);
     }
 
