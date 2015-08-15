119879cea2a5a94d754124cd61398443a117d11f
keattiwut <keattiwut.k@gmail.com>
2015-08-15 15:33:04 +0700

    test build

--- src/main/java/th/workshop/Application.java
+++ src/main/java/th/workshop/Application.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     private Application(){
-
     }
 
     public static void main(String[] args){
