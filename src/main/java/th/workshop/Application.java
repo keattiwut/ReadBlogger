88759200eaaca55a9dc66e11d99b8422032355e3
keattiwut <keattiwut.k@gmail.com>
2015-08-15 14:31:15 +0700

    add source.

---
+++ src/main/java/th/workshop/Application.java
@@ -0,0 +1,17 @@
+package th.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by DellLaptop on 15/08/2015.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(Application.class);
+    }
+
+}
