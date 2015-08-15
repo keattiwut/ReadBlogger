3ccbe600f4d8d2ef760d7ab36d64dc33a565668e
keattiwut <keattiwut.k@gmail.com>
2015-08-15 16:24:14 +0700

    remove import not used

--- src/main/java/th/workshop/BloggerController.java
+++ src/main/java/th/workshop/BloggerController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Method;
-
 /**
  * Created by DellLaptop on 15/08/2015.
  */
